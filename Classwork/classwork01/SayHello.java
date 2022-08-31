31601c2994616aef9b116117b474257fe4b3fe88
zleodai <ldai3@lion.lmu.edu>
2022-08-31 15:05:13 -0700

    HelloSayer for Hello World, Say Hello for the assignment

--- Classwork/classwork01/SayHello.java
+++ Classwork/classwork01/SayHello.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;  
 
-class HelloSayer {
-	public static void main(String args[]){
-	
-	Scanner myInput = new Scanner( System.in );
+class SayHello {
+    public static void main(String args[]){
+    
+    Scanner myInput = new Scanner( System.in );
 
-	System.out.println("What is thy name?");
-  	String input = myInput.nextLine();
+    System.out.println("What is thy name?");
+        String input = myInput.nextLine();
 
-	System.out.println("\n\n\n Hello " + input + "\n\n\n");
+    System.out.println("\n\n\n Hello " + input + "\n\n\n");
 
-	}
+    }
 }
