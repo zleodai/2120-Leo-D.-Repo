f423ab7001cbea7c94cf209f0bffd992ad6d01b4
zleodai <ldai3@lion.lmu.edu>
2022-09-07 13:59:57 -0700

    homework + classwork

---
+++ Classwork/classwork01/PiSolver.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+
+class PiSolver {
+    public static void main(String args[]){
+        static double RandomNumber(){
+            Random  = new Random();
+            double double_random = rand.nextDouble();
+            return double_random;
+        }
+        
+        System.out.println("Output: " + RandomNumber());
+        System.out.println("Output: " + RandomNumber());
+
+    }
+}
---
+++ Homework/homework01/SayHello.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;  
+
+class SayHello {
+    public static void main(String args[]){
+    
+    Scanner myInput = new Scanner( System.in );
+
+    System.out.println("What is thy name?");
+        String input = myInput.nextLine();
+
+    System.out.println("\n\n\n Hello " + input + "\n\n\n");
+
+    }
+}
